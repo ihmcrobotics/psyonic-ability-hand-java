92e29faa10176adfb606bc28bf1bf2ef8d9d000d
Dexton Anderson <danderson@ihmc.org>
2024-06-27 17:07:20 -0500

    Add publishing info

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,31 @@ repositories {
    maven { url = uri("https://jitpack.io") }
 }
 
+publishing {
+   publications {
+      create<MavenPublication>("mavenJava") {
+         from(components["java"])
+
+         groupId = project.group.toString()
+         artifactId = "psyonic-ability-hand-java"
+         version = project.version.toString()
+      }
+   }
+
+   repositories {
+      maven {
+         val releasesRepo = uri("https://s01.oss.sonatype.org/content/repositories/releases")
+         val snapshotsRepo = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+         url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepo else releasesRepo
+
+         credentials {
+            username = project.findProperty("publishUsername").toString()
+            password = project.findProperty("publishPassword").toString()
+         }
+      }
+   }
+}
+
 dependencies {
    implementation("com.github.weliem.blessed-bluez:blessed:0.61") {
       isTransitive = true
