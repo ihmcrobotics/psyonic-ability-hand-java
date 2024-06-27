e13345fe548582163b7fbd16b0994afc835b1e96
Dexton Anderson <danderson@ihmc.org>
2024-06-27 16:33:46 -0500

    Formatting

--- src/main/java/us/ihmc/abilityhand/AbilityHandIndividualFingerControlCommand.java
+++ src/main/java/us/ihmc/abilityhand/AbilityHandIndividualFingerControlCommand.java
@@ -8,29 +8,28 @@ import java.nio.ByteOrder;
    Finger order is the same as in all other instances of ordered per-finger data
       (index,middle,ring,pinky,thumb flexor, thumb rotator).
  */
-public class AbilityHandIndividualFingerControlCommand {
+public class AbilityHandIndividualFingerControlCommand
+{
    private static final byte INDIVIDUAL_FINGER_CONTROL_COMMAND_ASCII_PREFIX = 'M';
 
-   //Position is set as an angle of the finger with respect to the palm
-   //Period is how fast the hand gets to that angle, the lower the period the faster
-   //Thumb Rotator goes inward the more negative the number
+   // Position is set as an angle of the finger with respect to the palm
+   // Period is how fast the hand gets to that angle, the lower the period the faster
+   // Thumb Rotator goes inward the more negative the number
    private float indexPosition = 5;
-   private float  indexPeriod = 0;
+   private float indexPeriod = 0;
    private float middlePosition = 5;
-   private float  middlePeriod = 0;
+   private float middlePeriod = 0;
    private float ringPosition = 5;
-   private float  ringPeriod = 0;
+   private float ringPeriod = 0;
    private float pinkyPosition = 5;
-   private float  pinkyPeriod = 0;
+   private float pinkyPeriod = 0;
    private float thumbFlexorPosition = 5;
-   private float  thumbFlexorPeriod = 0;
+   private float thumbFlexorPeriod = 0;
    private float thumbRotatorPosition = 50;
-   private float  thumbRotatorPeriod = 0;
+   private float thumbRotatorPeriod = 0;
 
-
-   // TODO:
-
-   public byte[] getAsciiSequence() {
+   public byte[] getAsciiSequence()
+   {
       ByteBuffer buffer = ByteBuffer.allocate(25);
       buffer.order(ByteOrder.LITTLE_ENDIAN);
 
@@ -56,50 +55,62 @@ public class AbilityHandIndividualFingerControlCommand {
 
       return buffer.array();
    }
+
    public void setIndexPosition(float indexPosition)
    {
       this.indexPosition = indexPosition;
    }
+
    public void setIndexPeriod(float indexPeriod)
    {
       this.indexPeriod = indexPeriod;
    }
+
    public void setMiddlePosition(float middlePosition)
    {
       this.middlePosition = middlePosition;
    }
+
    public void setMiddlePeriod(float middlePeriod)
    {
       this.middlePeriod = middlePeriod;
    }
+
    public void setRingPosition(float ringPosition)
    {
       this.ringPosition = ringPosition;
    }
+
    public void setRingPeriod(float ringPeriod)
    {
       this.ringPeriod = ringPeriod;
    }
+
    public void setPinkyPosition(float pinkyPosition)
    {
       this.pinkyPosition = pinkyPosition;
    }
+
    public void setPinkyPeriod(float pinkyPeriod)
    {
       this.pinkyPeriod = pinkyPeriod;
    }
+
    public void setThumbFlexorPosition(float thumbFlexorPosition)
    {
       this.thumbFlexorPosition = thumbFlexorPosition;
    }
+
    public void setThumbFlexorPeriod(float thumbFlexorPeriod)
    {
       this.thumbFlexorPeriod = thumbFlexorPeriod;
    }
+
    public void setThumbRotatorPosition(float thumbRotatorPosition)
    {
       this.thumbRotatorPosition = thumbRotatorPosition;
    }
+
    public void setThumbRotatorPeriod(float thumbRotatorPeriod)
    {
       this.thumbRotatorPeriod = thumbRotatorPeriod;
--- src/main/java/us/ihmc/abilityhand/example/AbilityHandIndividualExample.java
+++ src/main/java/us/ihmc/abilityhand/example/AbilityHandIndividualExample.java
@@ -3,46 +3,47 @@ package us.ihmc.abilityhand.example;
 import us.ihmc.abilityhand.AbilityHandBLEManager;
 import us.ihmc.abilityhand.AbilityHandIndividualFingerControlCommand;
 
-import java.util.Random;
+public class AbilityHandIndividualExample
+{
+   private static volatile boolean running = true;
 
-public class AbilityHandIndividualExample {
-    private static final Random RANDOM = new Random();
-    private static volatile boolean running = true;
+   public static void main(String[] args) throws InterruptedException
+   {
+      String handAddress = "DE:76:4F:34:6F:E1";
 
-    public static void main(String[] args) throws InterruptedException {
-        String handAddress = "DE:76:4F:34:6F:E1";
+      AbilityHandBLEManager bleManager = new AbilityHandBLEManager(new String[] {handAddress});
 
-        AbilityHandBLEManager bleManager = new AbilityHandBLEManager(new String[] {handAddress});
+      bleManager.connect();
 
-        bleManager.connect();
+      Runtime.getRuntime().addShutdownHook(new Thread(() ->
+      {
+         running = false;
+         bleManager.disconnect();
+      }));
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() ->
-        {
-            running = false;
-            bleManager.disconnect();
-        }));
-        AbilityHandIndividualFingerControlCommand individualCommand = new AbilityHandIndividualFingerControlCommand();
-        individualCommand.setIndexPosition(0);
-        individualCommand.setMiddlePosition(0);
-        individualCommand.setRingPosition(0);
-        individualCommand.setPinkyPosition(0);
-        bleManager.sendIndividualCommand(handAddress, individualCommand);
-        individualCommand.setIndexPeriod(0.0F);
-        individualCommand.setMiddlePeriod(0.0F);
-        individualCommand.setRingPeriod(0.0F);
-        individualCommand.setPinkyPeriod(0.0F);
-        while(running)
-        {
-            for(int i = 0; i < 90; i++)
-            {
-                individualCommand.setIndexPosition((float) Math.abs(60 * Math.sin(i)));
-                individualCommand.setMiddlePosition((float) Math.abs(60 * Math.cos(i)));
-                individualCommand.setRingPosition((float) Math.abs(60 * Math.sin(i)));
-                individualCommand.setPinkyPosition((float) Math.abs(60 * Math.cos(i)));
-                bleManager.sendIndividualCommand(handAddress, individualCommand);
-                Thread.sleep(100);
-            }
-            Thread.sleep(250);
-        }
-    }
+      AbilityHandIndividualFingerControlCommand individualCommand = new AbilityHandIndividualFingerControlCommand();
+      individualCommand.setIndexPosition(0);
+      individualCommand.setMiddlePosition(0);
+      individualCommand.setRingPosition(0);
+      individualCommand.setPinkyPosition(0);
+      bleManager.sendIndividualCommand(handAddress, individualCommand);
+      individualCommand.setIndexPeriod(0.0F);
+      individualCommand.setMiddlePeriod(0.0F);
+      individualCommand.setRingPeriod(0.0F);
+      individualCommand.setPinkyPeriod(0.0F);
+
+      while (running)
+      {
+         for (int i = 0; i < 90; i++)
+         {
+            individualCommand.setIndexPosition((float) Math.abs(60 * Math.sin(i)));
+            individualCommand.setMiddlePosition((float) Math.abs(60 * Math.cos(i)));
+            individualCommand.setRingPosition((float) Math.abs(60 * Math.sin(i)));
+            individualCommand.setPinkyPosition((float) Math.abs(60 * Math.cos(i)));
+            bleManager.sendIndividualCommand(handAddress, individualCommand);
+            Thread.sleep(100);
+         }
+         Thread.sleep(250);
+      }
+   }
 }
