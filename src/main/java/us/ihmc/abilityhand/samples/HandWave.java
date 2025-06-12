2a7c10dd0b70680a5bb8e4177d64529078138e2d
Tomasz Bialek <tbialek@ihmc.org>
2025-06-12 09:16:19 -0500

    Fixed bug in HandWave.java + added comments

--- src/main/java/us/ihmc/abilityhand/samples/HandWave.java
+++ src/main/java/us/ihmc/abilityhand/samples/HandWave.java
@@ -5,18 +5,19 @@ import us.ihmc.abilityhand.FloatArray6;
 import us.ihmc.abilityhand.global.abilityhand;
 import us.ihmc.abilityhand.library.AbilityHandAPINativeLibrary;
 
-import java.time.Instant;
-
 public class HandWave
 {
    public static void main(String[] args)
    {
+      // Library must be loaded before use
       boolean loaded = AbilityHandAPINativeLibrary.load();
       assert loaded;
 
-      AHWrapper wrapper = new AHWrapper((byte) 0x50, 921600);
+      // Initialize wrapper and connect
+      AHWrapper wrapper = new AHWrapper((byte) 0x50, 460800);
       wrapper.connect();
 
+      // Initialize the command
       FloatArray6 command = new FloatArray6();
       for (int i = 0; i < 5; ++i)
          command.put(i, 30.0f);
@@ -24,12 +25,16 @@ public class HandWave
 
       for (int i = 0; i < 100000; ++i)
       {
+         // Calculate hand wave
+         double timeSeconds = 1E-9 * System.nanoTime();
          for (int j = 0; j < command.size(); ++j)
          {
-            double ft = Instant.now().getEpochSecond() * 3.0 + j * (2.0 * Math.PI / 12.0);
+            double ft = timeSeconds * 3.0 + j * (2.0 * Math.PI / 12.0);
             command.put(j, (float) (0.5 * Math.sin(ft) + 0.5) * 45.0f + 15.0f);
          }
          command.put(5, -command.get(5));
+
+         // Write to the hand (also reads)
          wrapper.read_write_once(command, abilityhand.POSITION, (byte) 0);
       }
 
