e68651fa7f98dbd8fce63f7f5140544edfb49f93
Tomasz Bialek <tbialek@ihmc.org>
2025-06-12 15:09:14 -0500

    Using ihmc-build plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,54 +1,18 @@
 plugins {
-   id("java-library")
-   id("maven-publish")
+   id("us.ihmc.ihmc-build")
 }
 
-group = "us.ihmc"
-version = "1.1.1"
+ihmc {
+   group = "us.ihmc"
+   version = "1.1.1"
+   vcsUrl = "https://github.com/ihmcrobotics/psyonic-ability-hand-java"
+   openSource = false
 
-repositories {
-   mavenCentral()
+   configureDependencyResolution()
+   configurePublications()
 }
 
-java {
-   withSourcesJar()
-}
-
-publishing {
-   publications {
-      create<MavenPublication>("mavenJava") {
-         from(components["java"])
-
-         groupId = project.group.toString()
-         artifactId = "psyonic-ability-hand-java"
-         version = project.version.toString()
-      }
-   }
-
-   repositories {
-      maven {
-         val releasesRepo = uri("https://s01.oss.sonatype.org/content/repositories/releases")
-         val snapshotsRepo = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-         url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepo else releasesRepo
-
-         credentials {
-            username = project.findProperty("publishUsername").toString()
-            password = project.findProperty("publishPassword").toString()
-         }
-      }
-   }
-}
-
-dependencies {
-   // Transitive dependencies
-   api("us.ihmc:javacpp:1.5.11-ihmc-2") {
-      isTransitive = true
-   }
-   api("us.ihmc:ihmc-native-library-loader:2.0.4") {
-      isTransitive = true
-   }
-}
-
-tasks.test {
-   useJUnitPlatform()
+mainDependencies {
+   api("us.ihmc:javacpp:1.5.11-ihmc-2")
+   api("us.ihmc:ihmc-native-library-loader:2.0.4")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
-rootProject.name = "psyonic-ability-hand-java"
+pluginManagement {
+   plugins {
+      id("us.ihmc.ihmc-build") version "1.1.1"
+   }
+}
 
+buildscript {
+   repositories {
+      maven { url = uri("https://plugins.gradle.org/m2/") }
+      mavenLocal()
+   }
+   dependencies {
+      classpath("us.ihmc:ihmc-build:1.1.1")
+   }
+}
