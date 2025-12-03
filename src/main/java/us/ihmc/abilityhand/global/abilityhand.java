826e70a851734fb3eb63d3f711ed374728d9c2b8
ihmc-rosie <rosie@ihmc.us>
2025-12-03 17:18:02 +0000

    Rebuild natives for Linux (x86_64)

--- src/main/java/us/ihmc/abilityhand/global/abilityhand.java
+++ src/main/java/us/ihmc/abilityhand/global/abilityhand.java
@@ -25,7 +25,11 @@ public class abilityhand extends us.ihmc.abilityhand.AbilityHandJavaAPIConfig {
 
 // #include "hand.h"
 // #include "ppp.h"
+// #ifdef PLATFORM_WINDOWS
+// #include "winserial.h"
+// #elif defined(PLATFORM_LINUX)
 // #include "linux_serial.h"
+// #endif
 
 @MemberGetter public static native @Cast("const uint16_t") short BUFFER_SIZE();
 @MemberGetter public static native @Cast("const uint16_t") short STUFFED_BUFFER_SIZE();
